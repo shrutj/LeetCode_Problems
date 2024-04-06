b95197c8917e2b53d23d4b80c220b457c3a76f71
Shrut Jain <129376990+shrutj@users.noreply.github.com>
2024-04-06 18:51:46 +0530

    Create 1148.Article_Views_I.sql

---
+++ 1148.Article_Views_I.sql
@@ -0,0 +1,6 @@
+-- # 1148. Problem Statement: Article Views I
+--                            Write a solution to find all the authors that viewed at least one of their own articles.
+
+--                            Return the result table sorted by id in ascending order.
+
+SELECT DISTINCT author_id AS id FROM Views WHERE author_id = viewer_id ORDER BY author_id ASC;
