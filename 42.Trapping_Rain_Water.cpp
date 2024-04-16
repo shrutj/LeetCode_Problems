8dc7fad17381079893bf4a1cf59c4d3933070a8b
Shrut Jain <129376990+shrutj@users.noreply.github.com>
2024-04-16 11:51:17 +0530

    Create 42.Trapping_Rain_Water.cpp

---
+++ 42.Trapping_Rain_Water.cpp
@@ -0,0 +1,40 @@
+// # 42. Problem Statement: Trapping Rain Water
+//                          Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int lmax = height[0];
+        int rmax = height[n-1];
+        int lpos = 1;
+        int rpos = n-2;
+        int water = 0;
+        while(lpos <= rpos)
+        {
+            if(height[lpos] >= lmax)
+            {
+                lmax = height[lpos];
+                lpos++;
+            }
+            else if(height[rpos] >= rmax)
+            {
+                rmax = height[rpos];
+                rpos--;
+            }
+            else if(lmax <= rmax && height[lpos] < lmax)
+            {
+                water += lmax - height[lpos];
+                lpos++;
+            }
+            else
+            {
+                water += rmax - height[rpos];
+                rpos--;
+            }
+        
+        }
+        return water;
+    }
+};
